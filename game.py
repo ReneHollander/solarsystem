d9ccee78ba458703271501eae8022bd01e7c2fab
Paul Kalauner <pkalauner@student.tgm.ac.at>
2015-11-05 09:56:50 +0100

    added mouse support for camera

--- game.py
+++ game.py
@@ -11,36 +11,23 @@ from util.camera import Camera
 pyglet.resource.path = ['resource/mesh']
 
 config = pyglet.gl.Config(sample_buffers=1, samples=8)
-window = pyglet.window.Window(800, 600, config=config, caption='Demo', resizable=True, vsync=False)
+window = pyglet.window.Window(800, 600, config=config, caption='Solarsystem', resizable=True, vsync=False)
 fps_display = pyglet.window.FPSDisplay(window)
-
 rotation = 0
 earth = Earth()
 lightfv = ctypes.c_float * 4
 time = 0
-
 orientation = Quaternion()
-print(orientation)
-
-keys = key.KeyStateHandler()
-window.push_handlers(keys)
-
-camera = Camera(keys)
-
-
-def on_mouse_motion(x, y, dx, dy):
-    camera.add_mouse_delta(dx, dy)
-
-
+camera = Camera(window)
 model_matrix = Matrix4()
 proj_matrix = None
+mvp = Matrix4()
 
 
 @window.event
 def on_resize(width, height):
     global proj_matrix
     proj_matrix = Matrix4.new_perspective(45, float(width) / float(height), 0.1, 100.0)
-    print(proj_matrix)
     glViewport(0, 0, width, height)
     glMatrixMode(GL_MODELVIEW)
     return True
@@ -49,9 +36,6 @@ def on_resize(width, height):
 @window.event
 def on_draw():
     window.clear()
-
-    mvp = proj_matrix * camera.view_matrix * model_matrix
-
     glLightfv(GL_LIGHT0, GL_POSITION, lightfv(-40, 200, 100, 0.0))
     glLightfv(GL_LIGHT0, GL_AMBIENT, lightfv(0.2, 0.2, 0.2, 1.0))
     glLightfv(GL_LIGHT0, GL_DIFFUSE, lightfv(0.5, 0.5, 0.5, 1.0))
@@ -69,6 +53,8 @@ def update(dt):
     global time
     time += dt * 60 * 60
     camera.update(dt)
+    global mvp
+    mvp = proj_matrix * camera.view_matrix * model_matrix
     earth.update(time)
 
 
--- util/camera.py
+++ util/camera.py
@@ -1,10 +1,13 @@
 from euclid import *
-from pyglet.window import key
+from pyglet.window import key, mouse, pyglet
 
 
 class Camera():
-    def __init__(self, keys):
-        self.keys = keys
+    def __init__(self, window):
+        self.window = window
+        self.keys = key.KeyStateHandler()
+        self.mouse_locked = False
+        window.push_handlers(self.on_mouse_press, self.on_mouse_motion, self.on_key_press, self.on_key_release)
 
         self.dx = 0
         self.dy = 0
@@ -17,38 +20,54 @@ class Camera():
         speed = 50
         speed *= delta / 1e1
 
-        rotSpeed = 1.5 * speed
+        rotSpeed = 0.5 * speed
 
         dx = self.get_dx()
         dy = self.get_dy()
 
         if dy != 0:
-            self.orientation = Quaternion.new_rotate_axis(0, Vector3(-dy * rotSpeed, 1, 0)) * self.orientation
+            self.orientation = Quaternion.new_rotate_axis(-dy * rotSpeed, Vector3(1, 0, 0)) * self.orientation
 
         if dx != 0:
-            self.orientation = Quaternion.new_rotate_axis(0, Vector3(dx * rotSpeed, 0, 1)) * self.orientation
+            self.orientation = Quaternion.new_rotate_axis(dx * rotSpeed, Vector3(0, 1, 0)) * self.orientation
 
         self.orientation.normalize()
 
         posDelta = Vector3()
-        if self.keys[key.W]:
-            posDelta.z += speed
-        if self.keys[key.S]:
-            posDelta.z -= speed
-        if self.keys[key.A]:
-            posDelta.x += speed
-        if self.keys[key.D]:
-            posDelta.x -= speed
-        if self.keys[key.SPACE]:
-            posDelta.y -= speed
-        if self.keys[key.LCTRL]:
-            posDelta.y += speed
+        if self.mouse_locked:
+            if self.keys[key.W]:
+                posDelta.z += speed
+            if self.keys[key.S]:
+                posDelta.z -= speed
+            if self.keys[key.A]:
+                posDelta.x += speed
+            if self.keys[key.D]:
+                posDelta.x -= speed
+            if self.keys[key.SPACE]:
+                posDelta.y -= speed
+            if self.keys[key.LCTRL]:
+                posDelta.y += speed
 
         inverse = self.orientation.get_matrix().inverse().get_quaternion()
         self.position += inverse * posDelta
 
         matrix = self.orientation.get_matrix().translate(self.position.x, self.position.y, self.position.z)
         self.view_matrix = matrix
+
+    def on_key_press(self, symbol, modifiers):
+        self.keys[symbol] = True
+        if symbol == key.ESCAPE:
+            self.window.set_exclusive_mouse(False)
+            self.mouse_locked = False
+            return pyglet.event.EVENT_HANDLED
+
+    def on_key_release(self, symbol, modifiers):
+        self.keys[symbol] = False
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        if button == mouse.LEFT:
+            self.window.set_exclusive_mouse(True)
+            self.mouse_locked = True
 
     def get_dx(self):
         tmp = self.dx
@@ -60,6 +79,7 @@ class Camera():
         self.dy = 0
         return tmp
 
-    def add_mouse_delta(self, dx, dy):
-        self.dx += dx
-        self.dy += dy
+    def on_mouse_motion(self, x, y, dx, dy):
+        if self.mouse_locked:
+            self.dx += dx
+            self.dy += dy
