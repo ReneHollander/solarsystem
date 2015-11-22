e90a6f5b0cf8916bbd00980e2f1ad72220b5fe62
ReneHollander <rene.hollander@hotmail.de>
2015-11-22 17:11:43 +0100

    more planets!

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ import ctypes
 import pyglet
 from euclid import *
 from pyglet.gl import *
-from solarsystem.body import Planet
+from solarsystem.body import OrbitingBody, StationaryBody
 from solarsystem.orbit import CircualOrbit
 from util.camera import Camera
 
@@ -11,33 +11,30 @@ pyglet.resource.path = ['resource/texture']
 config = pyglet.gl.Config(sample_buffers=1, samples=8)
 window = pyglet.window.Window(800, 600, config=config, caption='Solarsystem', resizable=True, vsync=False)
 fps_display = pyglet.window.FPSDisplay(window)
-rotation = 0
-lightfv = ctypes.c_float * 4
-time = 0
-orientation = Quaternion()
+
 camera = Camera(window)
 model_matrix = Matrix4()
 proj_matrix = None
 mvp = Matrix4()
 
-orbitmod = 10000000.0
+time = 0
+
+orbitmod = 1000000.0
 radiusmod = 1000.0
-
-print(149597500 / orbitmod)
-
-earth = Planet(None, "earth", CircualOrbit(149597500 / orbitmod, 365.256363 * 24 * 60 * 60), 6371 / radiusmod,
-               23.4392811,
-               0.99726968 * 24 * 60 * 60)
-moon = Planet(earth, "moon", CircualOrbit(384000 / orbitmod, 29.530589 * 24 * 60 * 60), 1737 / radiusmod, 6.687,
-              27.321582 * 24 * 60 * 60)
-
-planets = [earth, moon]
+dts = 24 * 60 * 60
+sun = StationaryBody(None, "sun", 12)
+mercury = OrbitingBody(sun, "mercury", 4879 / radiusmod, CircualOrbit(57909050 / orbitmod, 87.969 * dts), 0.034, 58.646 * dts)
+venus = OrbitingBody(sun, "venus", 6051 / radiusmod, CircualOrbit(108939000 / orbitmod, 224.701 * dts), 2.64, -243.025 * dts)
+earth = OrbitingBody(sun, "earth", 6371 / radiusmod, CircualOrbit(149597500 / orbitmod, 365.256363 * dts), 23.4392811, 0.99726968 * dts)
+moon = OrbitingBody(earth, "moon", 1737 / radiusmod, CircualOrbit(3840000 * 4 / orbitmod, 29.530589 * dts), 6.687, 27.321582 * dts)
+mars = OrbitingBody(sun, "mars", 3398 / radiusmod, CircualOrbit(225000000 / orbitmod, 686.971 * dts), 25.19, 1.025957 * dts)
+planets = [sun, mercury, venus, earth, moon, mars]
 
 
 @window.event
 def on_resize(width, height):
     global proj_matrix
-    proj_matrix = Matrix4.new_perspective(45, float(width) / float(height), 0.1, 100.0)
+    proj_matrix = Matrix4.new_perspective(45, float(width) / float(height), 0.1, 1000.0)
     glViewport(0, 0, width, height)
     glMatrixMode(GL_MODELVIEW)
     return True
@@ -46,7 +43,8 @@ def on_resize(width, height):
 @window.event
 def on_draw():
     window.clear()
-    # glLightfv(GL_LIGHT0, GL_POSITION, lightfv(-40, 200, 100, 0.0))
+    lightfv = ctypes.c_float * 4
+    # glLightfv(GL_LIGHT0, GL_POSITION, lightfv(0, 0, 0, 0.0))
     # glLightfv(GL_LIGHT0, GL_AMBIENT, lightfv(0.2, 0.2, 0.2, 1.0))
     # glLightfv(GL_LIGHT0, GL_DIFFUSE, lightfv(0.5, 0.5, 0.5, 1.0))
     # glEnable(GL_LIGHT0)
@@ -57,10 +55,6 @@ def on_draw():
 
     for planet in planets:
         planet.render(mvp.__copy__())
-
-    # earth.render(mvp.__copy__())
-    # mvp.translate(40, 0, 0)
-    # moon.render(mvp.__copy__())
 
     fps_display.draw()
 
--- solarsystem/body.py
+++ solarsystem/body.py
@@ -1,5 +1,5 @@
 import math
-from abc import ABCMeta
+from abc import ABCMeta, abstractmethod
 from euclid import Vector3
 from pyglet.gl import *
 from util.mathhelper import toGlMatrix
@@ -7,24 +7,43 @@ from util.texture import Texture
 
 
 class Body(object, metaclass=ABCMeta):
-    def __init__(self, parent):
+    def __init__(self, parent, name, radius):
         self.parent = parent
-
-
-class Planet(Body, metaclass=ABCMeta):
-    def __init__(self, parent, name, orbit, mean_radius, axial_tilt, sidereal_rotation_period):
-        super().__init__(parent)
-
         self.name = name
-        self.orbit = orbit
-        self.mean_radius = mean_radius
-        self.axial_tilt = axial_tilt
-        self.sidereal_rotation_period = sidereal_rotation_period
+        self.radius = radius
 
         self.texture = Texture(self.name.lower() + ".jpg")
         self.sphere = gluNewQuadric()
         gluQuadricNormals(self.sphere, GLU_SMOOTH)
         gluQuadricTexture(self.sphere, GL_TRUE)
+
+    def update(self, time):
+        pass
+
+    @abstractmethod
+    def render(self, matrix):
+        pass
+
+
+class StationaryBody(Body, metaclass=ABCMeta):
+    def __init__(self, parent, name, radius, xyz=Vector3()):
+        super().__init__(parent, name, radius)
+        self.xyz = xyz
+
+    def render(self, matrix):
+        matrix.translate(self.xyz.x, self.xyz.z, self.xyz.y)
+        glLoadMatrixd(toGlMatrix(matrix))
+        self.texture.draw()
+        gluSphere(self.sphere, self.radius, 50, 50)
+        glDisable(GL_TEXTURE_2D)
+
+
+class OrbitingBody(Body, metaclass=ABCMeta):
+    def __init__(self, parent, name, radius, orbit, axial_tilt, sidereal_rotation_period):
+        super().__init__(parent, name, radius)
+        self.orbit = orbit
+        self.axial_tilt = axial_tilt
+        self.sidereal_rotation_period = sidereal_rotation_period
 
     def update(self, time):
         self.timefactor = (time % self.sidereal_rotation_period) / self.sidereal_rotation_period
@@ -34,17 +53,11 @@ class Planet(Body, metaclass=ABCMeta):
             self.xyz = self.orbit.calculate(time)
 
     def render(self, matrix):
-        # matrix.translate(0, 0, -20)
         matrix.translate(self.xyz.x, self.xyz.z, self.xyz.y)
         matrix.rotate_axis(math.radians(-90), Vector3(1, 0, 0))
         matrix.rotate_axis(math.radians(self.axial_tilt), Vector3(0, 1, 0))
         matrix.rotate_axis(math.radians(-360 * self.timefactor), Vector3(0, 0, 1))
         glLoadMatrixd(toGlMatrix(matrix))
         self.texture.draw()
-        gluSphere(self.sphere, self.mean_radius, 50, 50)
+        gluSphere(self.sphere, self.radius, 50, 50)
         glDisable(GL_TEXTURE_2D)
-
-    def __str__(self):
-        return self.name + "({orbit: " + str(self.orbit) + ", mean_radius: \"" + str(
-            self.mean_radius) + "\", axial_tilt: \"" + str(self.axial_tilt) + "\", sidereal_rotation_period: \"" + str(
-            self.sidereal_rotation_period) + "})"
